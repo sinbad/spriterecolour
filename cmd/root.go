9a62cce2c1b31316105e0ea996e06cb1fc8f8728
Steve Streeting <steve@stevestreeting.com>
2016-11-10 10:30:10 +0000

    WIP

---
+++ cmd/root.go
@@ -0,0 +1,120 @@
+// Copyright © 2016 Steve Streeting
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/sinbad/spriterecolour/recolour"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	outputFile        string
+	outputParamsFile  string
+	outputTextureFile string
+)
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd *cobra.Command
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	RootCmd = &cobra.Command{
+		Use:   "spriterecolour",
+		Short: "Make re-colourable sprites from original authored sprites",
+		Long: `SpriteRecolour takes an input sprite, and generates a new template
+sprite from that which indexes a list of hues which can be changed to recolour
+it at runtime with a shader. The original hues are generated as a 1D texture and
+as a list of RGBs depending on whether you want to use a texture or shader
+parameters to control the recolouring.`,
+		Run: rootCommand,
+	}
+
+	RootCmd.Flags().StringVarP(&outputFile, "output", "o", "", "Output sprite file; default <input>_template.png")
+	RootCmd.Flags().StringVarP(&outputTextureFile, "texture", "t", "", "File to write palette as texture; default <input>_palette.png")
+	RootCmd.Flags().StringVarP(&outputParamsFile, "params", "p", "", "File to write shader params to; default none, stdout")
+	RootCmd.SetUsageFunc(usageCommand)
+
+}
+
+func usageCommand(cmd *cobra.Command) error {
+	usage := `
+Usage:
+  spriterecolour [options] <input file>
+
+Options:
+  -o, --output string    Output sprite file; default <input>_template.png
+  -p, --params string    File to write shader params to; default none, stdout
+  -t, --texture string   File to write palette as texture; default <input>_palette.png
+`
+	fmt.Fprintf(os.Stderr, usage)
+	return nil
+}
+
+func rootCommand(cmd *cobra.Command, args []string) {
+	if len(args) == 0 {
+		fmt.Fprintf(os.Stderr, "Required: input sprite image\n")
+		RootCmd.Usage()
+		os.Exit(1)
+	}
+	infile := args[0]
+
+	if len(outputFile) == 0 {
+		outputFile = filepath.Join(filepath.Dir(infile),
+			fmt.Sprintf("%s_template.png", filepath.Base(infile)))
+	}
+
+	if len(outputTextureFile) == 0 {
+		outputTextureFile = filepath.Join(filepath.Dir(infile),
+			fmt.Sprintf("%s_palette.png", filepath.Base(infile)))
+	}
+
+	palette, err := recolour.Generate(infile, outputFile, outputTextureFile, outputParamsFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(9)
+	}
+
+	fmt.Fprintf(os.Stderr, "Completed successfully\n")
+	fmt.Fprintf(os.Stderr, "  Sprite template: %v\n", outputFile)
+	fmt.Fprintf(os.Stderr, "  Palette texture: %v\n", outputTextureFile)
+	if len(outputParamsFile) > 0 {
+		fmt.Fprintf(os.Stderr, "  Params:%v\n", outputParamsFile)
+	} else {
+		for _, c := range palette {
+			// TODO actually generate shader code?
+			fmt.Printf("%v\n", c)
+		}
+	}
+
+}
---
+++ recolour/generate.go
@@ -0,0 +1,138 @@
+package recolour
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"os"
+	"sort"
+
+	colorful "github.com/lucasb-eyer/go-colorful"
+	// This causes the codecs to be loaded
+	_ "image/gif"
+	_ "image/jpeg"
+	"image/png"
+)
+
+var EPSILON float64 = 0.00000001
+
+func floatEquals(a, b float64) bool {
+	if (a-b) < EPSILON && (b-a) < EPSILON {
+		return true
+	}
+	return false
+}
+
+type UniqueColour struct {
+	RGBA    color.NRGBA
+	H, S, V float64
+	// Store an index so that references in map know final position in list
+	Index uint16
+}
+
+type UniqueColourList []*UniqueColour
+
+func (l UniqueColourList) Len() int {
+	return len(l)
+}
+
+func (l UniqueColourList) Less(i, j int) bool {
+	a := l[i]
+	b := l[j]
+	if floatEquals(a.H, b.H) {
+		if floatEquals(a.S, b.S) {
+			return a.V < b.V
+		}
+		return a.S < b.S
+	}
+	return a.H < b.H
+}
+
+func (l UniqueColourList) Swap(i, j int) {
+	l[i].Index, l[j].Index = l[j].Index, l[i].Index
+	l[i], l[j] = l[j], l[i]
+}
+
+// Generate reads an input sprite texture and generates a reference sprite file,
+// and a base lookup texture and / or parameter list
+func Generate(imagePath, outImagePath, outPaletteTexture, outPaletteParams string) (color.Palette, error) {
+
+	f, err := os.OpenFile(imagePath, os.O_RDONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+
+	return GenerateFromImage(img, outImagePath, outPaletteTexture, outPaletteParams)
+}
+
+// GenerateFromImage reads an image and generates a reference sprite file,
+// and a base lookup texture and / or parameter list
+func GenerateFromImage(img image.Image, outImagePath, outPaletteTexture, outPaletteParams string) (color.Palette, error) {
+	// Go colours are alpha-premultiplied and uint32's with 65535 range: weird
+	// We want NON alpha premultiplied by default (internally could be premultiplied)
+	rgba := image.NewNRGBA(img.Bounds())
+	bounds := rgba.Bounds()
+	// Record of what colours are present
+	colourMap := make(map[color.NRGBA]*UniqueColour)
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			p := rgba.NRGBAAt(x, y)
+
+			if _, ok := colourMap[p]; !ok {
+				cfcol := colorful.Color{float64(p.R) / 255.0, float64(p.G) / 255.0, float64(p.B) / 255.0}
+				h, s, v := cfcol.Hsv()
+				col := &UniqueColour{p, h, s, v, 0}
+				colourMap[p] = col
+			}
+		}
+	}
+
+	if len(colourMap) > 65536 {
+		return nil, fmt.Errorf("Sorry, sprite contains too many colours")
+	}
+
+	// Re-order the colours by HSV so easier to edit
+	colourList := make(UniqueColourList, 0, len(colourMap))
+	nextIndex := uint16(0)
+	for _, c := range colourMap {
+		c.Index = nextIndex
+		colourList = append(colourList, c)
+		nextIndex++
+	}
+	// Sort, the swap function will swap indexes
+	sort.Sort(colourList)
+
+	// Now generate the sprite output
+	outSprite := image.NewNRGBA(image.Rect(bounds.Min.X, bounds.Min.Y, bounds.Max.X, bounds.Max.Y))
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			inpix := rgba.NRGBAAt(x, y)
+
+			// Should never fail but just don't write pixel if it does
+			if col, ok := colourMap[inpix]; ok {
+				// Red channel = colour index U
+				red := uint8(col.Index & 0x0000FFFF)
+				// Blue channel = colour index V
+				blue := uint8(col.Index >> 16)
+				// Green channel = unused for now
+				outSprite.Set(x, y, color.NRGBA{red, blue, 0, inpix.A})
+			}
+		}
+	}
+	of, err := os.OpenFile(outImagePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		return nil, err
+	}
+	err = png.Encode(of, outSprite)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
